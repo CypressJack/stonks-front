cb0b79b56b934dcbebae0c2db509761b2736c4c7
Brandon <copelandbrandon2@gmail.com>
2021-07-04 11:13:00 -0600

    implemented button component and stories

--- stories/index.jsx
+++ stories/index.jsx
@@ -3,15 +3,11 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { StylesProvider } from "@material-ui/core/styles";
 
-
-import Button from "@material-ui/core/Button";
-
-
-// Import global styling overrides aka our theme
-import "../src/globalStyleOverride.css";
-
+// import Button from "@material-ui/core/Button";
+import Button from "components/Button";
 
 storiesOf("Button", module)
   .add("Base", () => <StylesProvider injectFirst><Button onClick={action("Button Clicked!")}>Base</Button></StylesProvider>)
   .add("Sell", () => <StylesProvider injectFirst><Button className="red-button" onClick={action("Button Clicked!")}>Sell</Button></StylesProvider> )
-  .add("Tutorial", () => <StylesProvider injectFirst><Button className="MuiButton-tutorial" onClick={action("Button Clicked!")}>Tutorial 1.</Button></StylesProvider>);
+  .add("Tutorial", () => <StylesProvider injectFirst><Button className="MuiButton-tutorial" onClick={action("Button Clicked!")} completed={false}>Tutorial 1.</Button></StylesProvider>)
+  .add("Completed tutorial", () => <StylesProvider injectFirst><Button className="MuiButton-tutorial" onClick={action("Button Clicked!")} completed={true} >Tutorial 1.</Button></StylesProvider>);
