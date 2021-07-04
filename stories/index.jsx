d3d1a799c9ff34e4bb540265e3e53b2a4501e2be
archaemedes <wadecroft@gmail.com>
2021-07-04 13:24:12 -0700

    import news stories

--- stories/index.jsx
+++ stories/index.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { StylesProvider } from "@material-ui/core/styles";
+import "./news.jsx";
 
 // import Button from "@material-ui/core/Button";
 import Button from "components/Button";
@@ -11,3 +12,4 @@ storiesOf("Button", module)
   .add("Sell", () => <StylesProvider injectFirst><Button className="red-button" onClick={action("Button Clicked!")}>Sell</Button></StylesProvider> )
   .add("Tutorial", () => <StylesProvider injectFirst><Button className="MuiButton-tutorial" onClick={action("Button Clicked!")} completed={false}>Tutorial 1.</Button></StylesProvider>)
   .add("Completed tutorial", () => <StylesProvider injectFirst><Button className="MuiButton-tutorial" onClick={action("Button Clicked!")} completed={true} >Tutorial 1.</Button></StylesProvider>);
+
