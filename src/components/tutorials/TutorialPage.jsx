eee7b520273a73b87a483f6571bff4e225a97e30
Brandon <copelandbrandon2@gmail.com>
2021-07-04 21:35:11 -0600

    implement tutorial page and story

--- src/components/tutorials/TutorialPage.jsx
+++ src/components/tutorials/TutorialPage.jsx
@@ -1,2 +1,17 @@
 import React from "react";
+import "./TutorialPage.scss";
 
+export default function TutorialPage(props) {
+
+
+  return (
+    <main className={"tutorial-main"}>
+      <header className={"tutorial-title"}>{props.title}</header>
+      <p className={"tutorial-paragraphs"}>{props.paragraph1}</p>
+      {props.subtitle1 && <header className={"subtitle"}>{props.subtitle1}</header>}
+      {props.paragraph2 && <p className={"tutorial-paragraphs"}>{props.paragraph2}</p>}
+      {props.subtitle2 && <header className={"subtitle"}>{props.subtitle2}</header>}
+      {props.paragraph3 && <p className={"tutorial-paragraphs"}>{props.paragraph3}</p>}
+    </main>
+  );
+};
