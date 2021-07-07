ce3f0994384b0aff2a7a63a93dc4a3ab4b8209d5
Brandon <copelandbrandon2@gmail.com>
2021-07-07 13:42:37 -0600

    improve searchbar and tutorials list

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Search from "@material-ui/icons/Search"
 import "./SearchBar.scss"
+import { TextField } from "@material-ui/core";
+import { green } from "@material-ui/core/colors";
 export default function SearchBar(props) {
-  // const [search, setSearch] = useState("");
+
 
   return (
     <div className={"background"}>
     <div className={"search-bar"}>
-      <input
+      <TextField
       className={"input"}
       value={props.search}
-      placeholder={"SEARCH"}
+      label={"SEARCH"}
       onChange={(event) => props.searchState(event.target.value)}
+      // variant={'outlined'}
+      size={'small'}
+      color={'green'}
       />
       <button search className={"button"} onClick={props.onClick}><Search/></button>
     </div>
