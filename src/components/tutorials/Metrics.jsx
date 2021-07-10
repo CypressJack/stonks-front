d708dbd4f18002e9a848303f9977258753dd676f
archaemedes <wadecroft@gmail.com>
2021-07-10 10:51:25 -0700

    add metric tut article

---
+++ src/components/tutorials/Metrics.jsx
@@ -0,0 +1,161 @@
+import React from "react";
+
+export default function MetricsTutorial(props) {
+  return (
+    <div>
+      <h3>What Are Metrics?</h3>
+      <p>
+        Metrics are measures of quantitative assessment commonly used for
+        assessing, comparing, and tracking performance or production. Generally,
+        a group of metrics will typically be used to build a dashboard that
+        management or analysts review on a regular basis to maintain performance
+        assessments, opinions, and business strategies.{" "}
+      </p>
+
+      <h3>Understanding Metrics</h3>
+      <p>
+        Metrics have been used in accounting, operations, and performance
+        analysis throughout history.
+      </p>
+
+      <p>
+        Metrics come in a wide range of varieties with industry standards and
+        proprietary models often governing their use.
+      </p>
+      <p>
+        Executives use them to analyze corporate finance and operational
+        strategies. Analysts use them to form opinions and investment
+        recommendations. Portfolio managers use metrics to guide their investing
+        portfolios. Furthermore, project managers also find them essential in
+        leading and managing strategic projects of all kinds.
+      </p>
+
+      <p>
+        Overall, metrics refer to a wide variety of data points generated from a
+        multitude of methods. Best practices across industries have created a
+        common set of comprehensive metrics used in ongoing evaluations.
+        However, individual cases and scenarios typically guide the choice of
+        metrics used.
+      </p>
+
+      <h3>Choosing Metrics</h3>
+      <p>
+        Every business executive, analyst, portfolio manager, and the project
+        manager has a range of data sources available to them for building and
+        structuring their own metric analysis. This can potentially make it
+        difficult to choose the best metrics needed for important assessments
+        and evaluations. Generally, managers seek to build a dashboard of what
+        has come to be known as key performance indicators (KPIs).
+      </p>
+
+      <p>
+        In order to establish a useful metric, a manager must first assess its
+        goals. From there, it is important to find the best outputs that measure
+        the activities related to these goals. A final step is also setting
+        goals and targets for KPI metrics that are integrated with business
+        decisions.
+      </p>
+
+      <p>
+        Academics and corporate researchers have defined many industry metrics
+        and methods that can help shape the building of KPIs and other metric
+        dashboards. An entire decision analysis method called applied
+        information economics was developed by Douglas Hubbard for analyzing
+        metrics in a variety of business applications. Other popular decision
+        analysis methods include cost-benefit analysis, forecasting, and Monte
+        Carlo simulation.
+      </p>
+
+      <p>
+        Several businesses have also popularized certain methods that have
+        become industry standards in many sectors. DuPont began using metrics to
+        better their own business and in the process came up with the popular
+        DuPont analysis which closely isolates variables involved in the return
+        on equity (ROE) metric. GE has also commissioned a set of metrics known
+        as Six Sigma that are commonly used today, with metrics tracked in six
+        key areas: critical to quality; defects; process capability; variation;
+        stable operations; and, design for Six Sigma.
+      </p>
+
+      <h3>Examples of Metrics</h3>
+      <p>
+        While there are a wide range of metrics, below are some commonly used
+        tools:
+      </p>
+
+      <i>Economic Metrics</i>
+
+      <ul>
+        <li>Gross domestic product (GDP)</li>
+        <li>Inflation</li>
+        <li>Unemployment rate</li>
+        <li>Operational Company Metrics</li>
+      </ul>
+
+      <p>
+        From a comprehensive perspective, executives, industry analysts, and
+        individual investors often look at key operational performance measures
+        of a company, all from different perspectives. Some top-level
+        operational metrics include measures derived from the analysis of a
+        company’s financial statements. Key financial statement metrics include
+        sales, earnings before interest and tax (EBIT), net income, earnings per
+        share, margins, efficiency ratios, liquidity ratios, leverage ratios,
+        and rates of return. Each of these metrics provides a different insight
+        into the operational efficiency of a company.
+      </p>
+
+      <p>
+        Executives use these operational metrics to make corporate decisions
+        involving costs, labor, financing, and investing. Executives and
+        analysts also build complex financial models to identify future growth
+        and value prospects, integrating both economic and operational metric
+        forecasts.
+      </p>
+
+      <p>
+        There are several metrics that are key to comparing the financial
+        position of companies against their competitors or the market overall.
+        Two of these key comparable metrics, which are based on market value,
+        include price-to-earnings ratio and price-to-book ratio.
+      </p>
+
+      <i>Portfolio Management</i>
+
+      <p>
+        Portfolio managers use metrics to identify investing allocations in a
+        portfolio. All types of metrics are also used for analyzing and
+        investing in securities that fit a specific portfolio strategy. For
+        example, environmental, social and governance (ESG) criteria are a set
+        of standards for a company's operations that socially conscious
+        investors use to screen potential investments.
+      </p>
+
+      <i>Project Management Metrics</i>
+
+      <p>
+        In project management, metrics are essential in measuring project
+        progression, output targets, and overall project success. Some of the
+        areas where metric analysis is often needed include resources, cost,
+        time, scope, quality, safety, and actions. Project managers have the
+        responsibility to choose metrics that provide the best analysis and
+        directional insight for a project. Metrics are followed in order to
+        measure the overall progression, production, and performance.
+      </p>
+
+      <h3>Three Key Takeaways</h3>
+      <ul>
+        <li>
+          Metrics are measures of quantitative assessment commonly used for
+          comparing, and tracking performance or production.
+        </li>
+        <li>Metrics can be used in a variety of scenarios.</li>
+        <li>
+          Metrics are heavily relied on in the financial analysis of companies
+          by both internal managers and external stakeholders.
+        </li>
+      </ul>
+      <h3>Reference</h3>
+      This article is an exerpt and belongs to <a href='https://www.investopedia.com/terms/m/metrics.asp'>Investopedia.com</a>
+    </div>
+  );
+}
--- src/components/tutorials/TutorialPage.jsx
+++ src/components/tutorials/TutorialPage.jsx
@@ -4,12 +4,16 @@ import Slide from '@material-ui/core/Slide';
 import Button from '../Button';
 import Check from '@material-ui/icons/Check'
 import KeyBoardReturn from '@material-ui/icons/KeyboardReturn';
+import Metrics from './Metrics';
 export default function TutorialPage(props) {
+
+  
   
   return (
     <Slide direction="up" in={true} mountOnEnter unmountOnExit>
     <main className={"tutorial-main"}>
-      <header className={"tutorial-title"}><b>{props.title}</b></header>
+    <header className={"tutorial-title"}><b>{props.title}</b></header>
+      {/* <header className={"tutorial-title"}><b>{props.title}</b></header>
       <p className={"tutorial-paragraphs"}>{props.paragraph1}</p>
       {props.subtitle1 && <header className={"subtitle"}><b>{props.subtitle1}</b></header>}
       {props.paragraph2 && <p className={"tutorial-paragraphs"}>{props.paragraph2}</p>}
@@ -18,7 +22,8 @@ export default function TutorialPage(props) {
       <div className={"back-button-container"}>
       <Button back sell onClick={props.onClick}><KeyBoardReturn/></Button>
       <Button back onClick={props.onComplete}><Check/></Button>
-      </div>
+      </div> */}
+      <Metrics/>
     </main>
     </Slide>
   );
